3d0ca3c0225d110fe144c51c1eedfde6de5c41d2
brandon-tol <brandon@thetoledanos.com>
2025-02-24 19:01:57 -0500

    Add IndoorNavigation component with initial layout

---
+++ expo-app/app/components/IndoorNavigation/IndoorNavigation.tsx
@@ -0,0 +1,15 @@
+import { Image, Text, View } from "react-native";
+
+const IndoorNavigation: React.FC = () => {
+
+    const position1 = {};
+    const position2 = {};
+    
+    return (
+        <View>
+            <img src="https://letsenhance.io/static/73136da51c245e80edc6ccfe44888a99/1015f/MainBefore.jpg" />
+        </View>
+    );
+};
+
+export default IndoorNavigation;
