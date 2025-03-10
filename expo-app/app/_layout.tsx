1b4b8c47aa75c566f8d37906c231c3c4b990bef5
Gderhy <gabrielderhy@gmail.com>
2025-03-09 21:11:55 -0400

    Add Smartlook analytics integration to RootLayout

--- expo-app/app/_layout.tsx
+++ expo-app/app/_layout.tsx
@@ -1,8 +1,18 @@
 import { Stack } from "expo-router";
 import { AuthProvider } from "@/app/contexts/AuthContext";
+import Smartlook from 'react-native-smartlook-analytics';
+import { useEffect } from "react";
+
 
 
 export default function RootLayout() {
+
+  useEffect(() => {
+    Smartlook.instance.preferences.setProjectKey(
+      "6cd84bb9390b30f9e14aaf82343928485690efe9"
+    );
+    Smartlook.instance.start();
+  }, []);
   return (
     <AuthProvider>
       <Stack screenOptions={{ headerShown: false }} />
